469ab457a4b8ec9866fbb952d51e0fdf79593cc5
Andrelise28 <andrelisebuttner@hotmail.com>
2022-08-30 22:01:22 -0300

    Adicionando textureLoader e imagem

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ camera.lookAt(0, 0, 0);
 
 const scene = new THREE.Scene();
 
-//gltf
+//glb
 const loader = new GLTFLoader();
 loader.load('../glb/wraith.glb', function(glb){
     console.log(glb);
@@ -51,21 +51,49 @@ scene.background = new THREE.CubeTextureLoader()
     ]);
 
 
-const texture = new THREE.TextureLoader().load('../texture/marioblockquest.png');
+const texture = new THREE.TextureLoader().load('../img/madeira.jpg');
 
-const material = new THREE.MeshBasicMaterial({ map: texture });
+const material = new THREE.MeshBasicMaterial({map: texture});
 
 
 let p1, p2, p3, p4, p5, p6, p7, p8, geometry, lines;
 
+//plano1
 p1 = new THREE.Vector3(-10, -10, 10);
 p2 = new THREE.Vector3(-10, 10, 10);
 p3 = new THREE.Vector3(10, 10, 10);
 p4 = new THREE.Vector3(10, -10, 10);
+
+//plano2
 p5 = new THREE.Vector3(-10, -10, -10);
 p6 = new THREE.Vector3(-10, 10, -10);
 p7 = new THREE.Vector3(10, 10, -10);
 p8 = new THREE.Vector3(10, -10, -10);
+
+//face da frente
+boxDraw([p3,p2,p1], geometry, scene, texture);
+boxDraw([p4,p3,p1], geometry, scene, texture);
+
+//face de cima
+boxDraw([p6,p2,p3], geometry, scene, texture);
+boxDraw([p7,p6,p3], geometry, scene, texture);
+
+//face da direita
+boxDraw([p7,p4,p8], geometry, scene, texture);
+boxDraw([p7,p3,p4], geometry, scene, texture);
+
+//face da esquerda
+boxDraw([p5,p1,p6], geometry, scene, texture);
+boxDraw([p1,p2,p6], geometry, scene, texture);
+
+//face de tras
+boxDraw([p5,p6,p7], geometry, scene, texture);
+boxDraw([p7,p8,p5], geometry, scene, texture);
+
+//face de baixo
+boxDraw([p1,p5,p4], geometry, scene, texture);
+boxDraw([p5,p8,p4], geometry, scene, texture);
+
 
 draw([p1, p2, p3, p4], geometry, lines, scene);
 draw([p5, p6, p7, p8], geometry, lines, scene);
@@ -74,11 +102,20 @@ draw([p2, p6], geometry, lines, scene);
 draw([p3, p7], geometry, lines, scene);
 draw([p4, p8], geometry, lines, scene);
 
+function boxDraw(points, geometry, scene, texture) {
+    geometry = new THREE.BufferGeometry().setFromPoints(points);
+    const materialBox = new THREE.MeshBasicMaterial({map: texture});
+    const mesh = new THREE.Mesh(geometry, materialBox); 
+    scene.add(mesh);
+}
+
 function draw(points, geometry, lines, scene) {
     geometry = new THREE.BufferGeometry().setFromPoints(points);
     lines = new THREE.LineLoop(geometry, material);
     scene.add(lines);
 }
+
+
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -98,6 +135,3 @@ function animate() {
     renderer.render(scene, camera);
 
 }
-
-
-
